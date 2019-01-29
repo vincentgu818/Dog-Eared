0a086d4d21ac95a64b37fc8ff9681a4f88cbd236
vincentgu818 <vincentgu818@gmail.com>
2019-01-29 11:57:40 -0500

    seeding api call #4

--- controllers/books_controller.js
+++ controllers/books_controller.js
@@ -15,7 +15,7 @@ router.use(session({
   saveUninitialized: false
 }))
 
-rp('https://www.googleapis.com/books/v1/volumes?q=Educated+Westover').
+rp('https://www.googleapis.com/books/v1/volumes?q=First+Conspiracy+Meltzer+Mensch').
   then((body) => {
     let jsonBody = JSON.parse(body)
      for(let googleBook of jsonBody.items) {
